c4cbf0934b1be25a1c0ee32c43e6eabc66f5cc8b
josephAlonzo <josep_alonzo@icloud.com>
2021-11-18 14:46:29 -0600

    added project

---
+++ src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>My user list</h1>
+    <v-row class="my-2" v-for="item in users" :key="item.id.value">
+      <v-col cols="12">
+        <v-card class="mx-auto" elevation="4">
+          <v-card-text>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  alt=""
+                  :src="item.picture.thumbnail"
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <h2>
+                  {{ item.name.title }} {{ item.name.last }} {{ item.name.first }}
+                </h2>
+                <v-divider class="mb-4"></v-divider>
+                <p class="mb-1">Email: {{ item.email }}</p>
+                <p>Phone Number: {{ item.cell }}</p>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card-text>
+          <v-card-actions>
+            <Modal :item=item class="mx-auto pb-4"></Modal>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+  
+</template>
+
+<script>
+import axios from "axios";
+import Modal from './Modal.vue'
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  components:{
+    Modal
+  },
+  methods: {
+    getList() {
+      axios.get("https://randomuser.me/api/?results=20").then((response) => {
+        this.users.push(...response.data.results);
+      });
+    },
+    scroll() {
+      window.onscroll = () => {
+        let bottomOfWindow = Math.max( window.pageYOffset,document.documentElement.scrollTop,document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight;
+        if (bottomOfWindow) {
+          this.getList();
+        }
+      };
+    },
+  },
+  created() {
+    this.getList();
+    this.scroll();
+  },
+};
+</script>
---
+++ src/views/Modal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="374px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark v-bind="attrs" v-on="on">
+            Plus d'informations
+          </v-btn>
+        </template>
+        <v-card width="374px">
+          <template slot="progress">
+            <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+            ></v-progress-linear>
+          </template>
+
+          <v-img
+            height="200"
+            :src="item.picture.large"
+          ></v-img>
+
+          <v-card-title>{{ item.name.title }} {{ item.name.last }} {{ item.name.first }}</v-card-title>
+          
+          <v-card-text class="mt-0">
+            <div class="mb-0 text-subtitle-1">{{item.gender}}</div>
+          </v-card-text>
+
+          <v-divider class="mx-2"></v-divider>
+
+          <v-card-title>Mes informations</v-card-title>
+
+          <v-card-text>
+            <div>
+              Adresse: <br>{{item.location.street.number}} {{item.location.street.name}} {{item.location.city}} {{item.location.state}} {{item.location.postcode}}
+              <br>Email: <br> {{item.email}}
+              <br>Téléphone: <br> {{item.phone}}
+            </div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="deep-purple lighten-2" text @click="dialog = false" class="mb-2">
+              Fermer
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </div>
+</template>
+
+<script>
+//text @click="dialog = false">
+export default {
+  name: "Modal",
+  props: ["item"],
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  created() {
+    console.log(this.item)
+  },
+};
+</script>
